c73e143eec0f3f2c7c5238fc113f796fbe94a1c4
Remi Boullerne <remi.boullerne@tag-games.com>
2019-01-26 16:38:21 +0000

    Added random chances to select a specific tier.

--- GGJ19/Assets/Scripts/EncounterSystem.cs
+++ GGJ19/Assets/Scripts/EncounterSystem.cs
@@ -8,8 +8,53 @@ public class EncounterSystem : MonoBehaviour
 {
     public const string k_folderPath = "Events";
 
+    public float[] TierChances = new float[] { 50f, 30f, 20f };
+
+    public int TotalTierChances
+    {
+        get {
+            int totalChances = 0;
+            foreach(int chance in TierChances)
+            {
+                totalChances += chance;
+            }
+            return totalChances;
+        }
+    }
+
+    public int MaxTier
+    {
+        get
+        {
+            return TierChances.Length - 1;
+        }
+    }
+
     [SerializeField]
     private Encounter[] mEncounters = null;
+
+    [SerializeField]
+    private List<int>[] mFreeIndicePerTier = new List<int>[3];
+
+    private int mHighestTier = 0;
+
+    public int HighestTier
+    {
+        get { return mHighestTier; }
+        set { mHighestTier = Mathf.Clamp(value, 0, MaxTier); }
+    }
+
+    public Encounter CurrentEncounter
+    {
+        get;
+        private set;
+    }
+
+    public void SolveCurrentEvent()
+    {
+        CurrentEncounter = GetNextEncounter(mHighestTier);
+        Debug.Log(string.Format("New encounter : {0} (tier {1})", CurrentEncounter.Id, CurrentEncounter.Tier));
+    }
 
     void Start()
     {
@@ -20,10 +65,14 @@ public class EncounterSystem : MonoBehaviour
             Debug.LogAssertion("No events could be loaded !");
         }
 
+        RefreshAllTiersIndice();
+
         Debug.Log(mEncounters.Length.ToString() + " encounter(s) loaded.");
+
+        CurrentEncounter = GetNextEncounter(1);
     }
 
-    public Encounter[] LoadData(string path)
+    private Encounter[] LoadData(string path)
     {
         TextAsset[] xmlFiles = Resources.LoadAll<TextAsset>(path);
 
@@ -57,5 +106,103 @@ public class EncounterSystem : MonoBehaviour
         }
 
         return null;
+    }
+
+    private int RandomlySelectTier(int maxTier)
+    {
+        // Total of tiers
+        float totalChances = 0f;
+        for(int tier = 0; tier <= maxTier; ++tier)
+        {
+            totalChances += TierChances[tier];
+        }
+
+        float[] normalizedChances = new float[maxTier];
+        normalizedChances[0] = TierChances[0] / totalChances;
+        for (int tier = 1; tier <= maxTier; ++tier)
+        {
+            normalizedChances[tier] = (TierChances[tier] / totalChances) + normalizedChances[tier - 1];
+        }
+
+        float random = Random.value;
+        for (int tier = maxTier; tier >= 0; --tier)
+        {
+            if(normalizedChances[tier] > random)
+            {
+                return tier;
+            }
+        }
+
+        // If we don't find a tier, we just throw a low one.
+        return 0;
+    }
+
+    private Encounter GetNextEncounter(int maxTier)
+    {
+        // Select a tier
+        int selectedTier = RandomlySelectTier(maxTier);
+        List<int> selectedTierList = mFreeIndicePerTier[selectedTier];
+
+        // If we used up all our encounter for this tier, refresh it.
+        if(selectedTierList.Count == 0)
+        {
+            selectedTierList = RefreshTierIndice(selectedTier);
+        }
+
+        // Select event from tier
+        int encounterIndicesIndex = Random.Range(0, selectedTierList.Count);
+
+        int encounterIndex = selectedTierList[encounterIndicesIndex];
+        selectedTierList.RemoveAt(encounterIndicesIndex);
+
+        return mEncounters[encounterIndex];
+    }
+
+    private List<int> RefreshTierIndice(int iTier)
+    {
+        // Create or clear tier indices
+        if (mFreeIndicePerTier[iTier] == null)
+        {
+            mFreeIndicePerTier[iTier] = new List<int>();
+        }
+        else
+        {
+            mFreeIndicePerTier[iTier].Clear();
+        }
+
+        for (int index = 0; index < mEncounters.Length; ++index)
+        {
+            Encounter encounter = mEncounters[index];
+            if (encounter.Tier == iTier)
+            {
+                mFreeIndicePerTier[iTier].Add(index);
+            }
+        }
+
+        return mFreeIndicePerTier[iTier];
+    }
+
+    private void RefreshAllTiersIndice()
+    {
+        // Create or clear all arrays
+        for (int index = 0; index < mFreeIndicePerTier.Length; ++index)
+        {
+            if (mFreeIndicePerTier[index] == null)
+            {
+                mFreeIndicePerTier[index] = new List<int>();
+            }
+            else
+            {
+                mFreeIndicePerTier[index].Clear();
+            }
+        }
+
+        for (int index = 0; index < mEncounters.Length; ++index)
+        {
+            Encounter encounter = mEncounters[index];
+            int encounterTier = Mathf.Clamp(encounter.Tier - 1, 0, MaxTier);
+
+            mFreeIndicePerTier[encounterTier].Add(index);
+        }
     }
 }
